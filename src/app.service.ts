279022d25a310f0b945bba87ce30aee4f4461382
khoavio <anhkhoaquachvo@gmail.com>
2024-01-25 09:38:58 +0700

    port 3002

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Develop - Auto Deploy';
+    return 'Hello World Develop - Auto Deploy 25/1';
   }
 }
