63e768898400bd1453f14287e9126075e3b5b4ce
khoavio <anhkhoaquachvo@gmail.com>
2024-01-25 16:00:08 +0700

    fix cors 3

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', ' PATCH', 'DELETE'],
-    // allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization',
     // credentials: true,
   });
   app.useGlobalInterceptors(new ResponseTransformInterceptor());
