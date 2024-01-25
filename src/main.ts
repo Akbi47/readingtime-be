d237e0d9fb349a20c197167f8a2b52803560eb08
khoavio <anhkhoaquachvo@gmail.com>
2024-01-25 15:27:56 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,11 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const allowedOrigins = [
-    'https://www.readingtime.vn',
-    'http://localhost:3000',
-    'http://localhost:3002',
-    'https://reading-time-six.vercel.app',
-  ];
+
   app.enableCors({
-    origin: allowedOrigins,
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', ' PATCH', 'DELETE'],
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
   });
   app.useGlobalInterceptors(new ResponseTransformInterceptor());
   app.useGlobalPipes(new BodyValidationPipe());
